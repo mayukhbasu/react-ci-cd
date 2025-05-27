f8bc624df0748fce8e20d4be195b7d84ceae8856
Mayukh Basu <roxy.firebird@gmail.com>
2025-05-27 19:12:55 +0530

    changes

--- src/features/users/userSlice.test.ts
+++ src/features/users/userSlice.test.ts
@@ -8,7 +8,7 @@ describe('users reducer', () => {
     loading: false,
     error: ''
   }
-  it('should handle initial state1', () => {
+  it('should handle initial state', () => {
     expect(usersReducer(undefined, {type: 'unknown'})).toEqual(initialState);
   });
 
